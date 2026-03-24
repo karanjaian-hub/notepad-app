18822943707fea111b0f6ef74f26b8a8bda3cbd1
karanjaian-hub <karanjaian420@gmail.com>
2026-03-24 16:10:39 +0300

    Add Dockerfile and environment variable configuration

--- src/main/java/com/notepad/notepad_app/MainVerticle.java
+++ src/main/java/com/notepad/notepad_app/MainVerticle.java
@@ -21,7 +21,7 @@ public class MainVerticle extends VerticleBase {
     // Railway sets these automatically when you add a PostgreSQL plugin
     // Locally they fall back to your laptop values
     String dbHost     = System.getenv().getOrDefault("PGHOST",     "localhost");
-    String dbPort     = System.getenv().getOrDefault("PGPORT",     "5432");
+    String dbPort     = System.getenv().getOrDefault("PGPORT",     "5433");
     String dbName     = System.getenv().getOrDefault("PGDATABASE", "notepad_db");
     String dbUser     = System.getenv().getOrDefault("PGUSER",     "postgres");
     String dbPassword = System.getenv().getOrDefault("PGPASSWORD", "your_password");
